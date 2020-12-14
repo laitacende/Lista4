0143acd40c93174357ecfdfbcce2ff4a0886e3ac
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-14 18:39:45 +0100

    implemented BoardTypes

---
+++ common/src/main/java/cs/checkers/common/BoardTypes.java
@@ -0,0 +1,11 @@
+package cs.checkers.common;
+
+/**
+ * BoardTypes
+ */
+public enum BoardTypes {
+  TwoPlayerChineseCheckers,
+  ThreePlayerChineseCheckers,
+  FourPlayerChineseCheckers,
+  SixPlayerChineseCheckers,
+}
