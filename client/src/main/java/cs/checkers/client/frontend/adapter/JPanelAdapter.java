8df74de22c7169ffde2ac7f7282d4562ee7af0bb
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-14 18:32:24 +0100

    created JPanelAdapter

---
+++ client/src/main/java/cs/checkers/client/frontend/adapter/JPanelAdapter.java
@@ -0,0 +1,14 @@
+package cs.checkers.client.frontend.adapter;
+
+import javax.swing.JPanel;
+
+/**
+ * JPanelAdapter
+ */
+public class JPanelAdapter extends JPanel {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+}
