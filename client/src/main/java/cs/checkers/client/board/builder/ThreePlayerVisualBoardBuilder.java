9d13e927776b157dba6a97460a7c08a6798b7cfa
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-16 19:46:33 +0100

    implemented pretty checker filling

--- client/src/main/java/cs/checkers/client/board/builder/ThreePlayerVisualBoardBuilder.java
+++ client/src/main/java/cs/checkers/client/board/builder/ThreePlayerVisualBoardBuilder.java
@@ -1,6 +1,9 @@
 package cs.checkers.client.board.builder;
 
+
+import cs.checkers.client.board.Coordinates;
 import cs.checkers.client.board.VisualBoard;
+import cs.checkers.client.board.VisualField;
 
 /**
  * implements {@link VisualBoardBuilder} for a Chinese Checkers game of 2
@@ -8,9 +11,14 @@ import cs.checkers.client.board.VisualBoard;
  */
 public class ThreePlayerVisualBoardBuilder extends VisualBoardBuilder {
 
-	@Override
-	protected void fillWithCheckers(VisualBoard boardToFill) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  protected void fillWithCheckers(VisualBoard boardToFill) {
+    VisualField fields[][] = boardToFill.getFields();
+    //upper triangle
+    fillTriangleWithCheckers(0, 12, 4, Coordinates.DOWN, fields, "P");
+    //lower right triangle
+    fillTriangleWithCheckers(9, 21, 4, Coordinates.DOWN, fields, "P");
+    //lower left triangle
+    fillTriangleWithCheckers(9, 3, 4, Coordinates.DOWN, fields, "P");
+  }
 }
