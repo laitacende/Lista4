7f79773cbacca98f75ab804ff1898de4ab5dc2ef
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-16 19:46:57 +0100

    implemented ugly checker filling

--- client/src/main/java/cs/checkers/client/board/builder/TwoPlayerVisualBoardBuilder.java
+++ client/src/main/java/cs/checkers/client/board/builder/TwoPlayerVisualBoardBuilder.java
@@ -1,6 +1,11 @@
 package cs.checkers.client.board.builder;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 import cs.checkers.client.board.VisualBoard;
+import cs.checkers.client.board.VisualChecker;
+import cs.checkers.client.board.VisualField;
 
 /**
  * implements {@link VisualBoardBuilder} for a Chinese Checkers game of 2
@@ -10,7 +15,28 @@ public class TwoPlayerVisualBoardBuilder extends VisualBoardBuilder {
 
   @Override
   protected void fillWithCheckers(VisualBoard boardToFill) {
-    // TODO Auto-generated method stub
+    VisualField fields[][] = boardToFill.getFields();
 
+    // upper corner
+    int incrementer = 1;
+    for (int row = 0; row < 4; row++) {
+      int numberOfAvailableFieldsOnSide = (fields[row].length - incrementer) / 2;
+      for (int column = numberOfAvailableFieldsOnSide; column <
+             numberOfAvailableFieldsOnSide + incrementer; column += 2) {
+        fields[row][column].setChecker(new VisualChecker("P"));
+      }
+      incrementer += 2;
+    }
+    // lower corner
+    incrementer -= 2;
+    for (int row = 13; row < 17; row++) {
+      int numberOfAvailableFieldsOnSide = (fields[row].length - incrementer) / 2;
+      
+      for (int column = numberOfAvailableFieldsOnSide; column <
+             numberOfAvailableFieldsOnSide + incrementer; column += 2) {
+        fields[row][column].setChecker(new VisualChecker("P"));
+      }
+      incrementer -= 2;
+    }
   }
 }
