635bff6e5cfe6d9641c39fc8d3c5ed00ed533ebe
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-17 01:37:04 +0100

    implemented GameInitializer

--- client/src/main/java/cs/checkers/client/game/GameInitializer.java
+++ client/src/main/java/cs/checkers/client/game/GameInitializer.java
@@ -1,27 +1,43 @@
 package cs.checkers.client.game;
 
+import java.io.OutputStream;
+import java.security.InvalidParameterException;
+
 import cs.checkers.client.board.AbstractVisualBoard;
 import cs.checkers.client.board.VisualBoard;
+import cs.checkers.client.board.factory.VisualBoardFactory;
+import cs.checkers.client.frontend.Controls;
+import cs.checkers.client.frontend.builder.GameWindowBuilder;
 import cs.checkers.common.BoardTypes;
 
 /**
  * initializes a game of Chinese Checkers
  */
 public class GameInitializer {
-  BoardTypes type;
-  VisualBoard board;
+  private VisualBoard board;
+  private OutputStream userOutput;
 
-  //TODO: Implement method
   public void initializeGraphic() {
+    Controls controls = new Controls(userOutput, board.getFields());
+    for (int row = 0; row < board.getFields().length; row++) {
+      for (int column = 0; row < board.getFields()[row].length; row++) {
+        board.getFields()[row][column].addMouseListener(controls);
+      }
+    }
+    GameWindowBuilder builder = new GameWindowBuilder();
+    builder.getGameWindow(board.getFields());
   }
 
-  //TODO: Implement method
-  AbstractVisualBoard initializeBoard() {
-    return new VisualBoard();
+  AbstractVisualBoard getBoard() {
+    return board;
   }
 
-  public GameInitializer(BoardTypes type) {
-    this.type = type;
+  public GameInitializer(BoardTypes type, OutputStream userOutput) throws InvalidParameterException {
+    this.userOutput = userOutput;
+    board = VisualBoardFactory.getBoard(type);
+    if (board == null) {
+      throw new InvalidParameterException();
+    }
   }
 
 }
