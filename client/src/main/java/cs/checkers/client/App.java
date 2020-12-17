6cf491c3c3bc9b0989c65ef1ad06338b9d217c85
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-17 11:39:24 +0100

    i think this is what the main() will look like

--- client/src/main/java/cs/checkers/client/App.java
+++ client/src/main/java/cs/checkers/client/App.java
@@ -1,8 +1,17 @@
 package cs.checkers.client;
 
+import javax.swing.JOptionPane;
+
+import cs.checkers.client.game.GameRunner;
+
 public class App {
-    
     public static void main(String[] args) {
+      GameRunner runner = new GameRunner();
+      if (!runner.initialize("localhost", 4999)) {
+        JOptionPane.showMessageDialog(null, "Initializing failed, exit");
+        return;
+      } else {
+        runner.run();
+      }
     }
-    
 }
