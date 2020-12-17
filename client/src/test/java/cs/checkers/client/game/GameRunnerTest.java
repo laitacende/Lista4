59a148f2c21ae7321f962d2c40e2744775db57b3
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-18 00:25:43 +0100

    temporarily disabled GameRunnerTest because it takes a long time

--- client/src/test/java/cs/checkers/client/game/GameRunnerTest.java
+++ client/src/test/java/cs/checkers/client/game/GameRunnerTest.java
@@ -2,13 +2,14 @@ package cs.checkers.client.game;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * GameRunnerTest
  */
 public class GameRunnerTest {
+  @Disabled
   @Test
   public void initializingGoesWrong() {
     GameRunner runner = new GameRunner();
