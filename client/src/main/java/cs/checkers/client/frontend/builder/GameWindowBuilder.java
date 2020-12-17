eb893527a03cb324fb3e073aa64f9032c90ac425
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-17 01:36:50 +0100

    implemented GameWindowBuilder

--- client/src/main/java/cs/checkers/client/frontend/builder/GameWindowBuilder.java
+++ client/src/main/java/cs/checkers/client/frontend/builder/GameWindowBuilder.java
@@ -2,6 +2,8 @@ package cs.checkers.client.frontend.builder;
 
 import cs.checkers.client.adapter.JButtonAdapter;
 import cs.checkers.client.frontend.GameWindow;
+import cs.checkers.client.frontend.adapter.JPanelAdapter;
+import java.awt.GridLayout;
 
 /**
  * build a {@link GameWindow} to be used for a game of Chinese Checkers
@@ -9,8 +11,16 @@ import cs.checkers.client.frontend.GameWindow;
    requires the field of a board to extend {@link JButtonAdapter}
  */
 public class GameWindowBuilder {
-  //TODO: Implement method
-  public GameWindow getGameWindow(JButtonAdapter[][] buttons) {
-    return new GameWindow();
+  public void getGameWindow(JButtonAdapter[][] buttons) {
+    JPanelAdapter board = new JPanelAdapter();
+    board.setLayout(new GridLayout(buttons.length, buttons[0].length));
+    for (int row = 0; row < buttons.length; row++) {
+      for (int column = 0; column < buttons[0].length; column++) {
+        board.add(buttons[row][column]);
+      }
+    }
+    GameWindow window = new GameWindow(board);
+    window.pack();
+    window.setVisible(true);
   }
 }
