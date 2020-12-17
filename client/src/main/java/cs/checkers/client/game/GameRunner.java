37209588e7929c5f7a3fe70540e967d963630af0
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-17 11:36:54 +0100

    changed GameRunner a bit

--- client/src/main/java/cs/checkers/client/game/GameRunner.java
+++ client/src/main/java/cs/checkers/client/game/GameRunner.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
 
 import cs.checkers.client.board.AbstractVisualBoard;
 import cs.checkers.client.networkIO.ServerHandler;
@@ -19,32 +18,52 @@ public class GameRunner {
   private Socket userSocket;
   private Scanner userInput;
   private boolean keepRunning;
+  private boolean initialized = false;
 
-  private void initialize() throws IOException {
+  /**
+   * use this function to connect {@link GameRunner} to a server and launch the
+   * game window
+   * 
+   * @param ip   ip of the server we're trying to connect to
+   * @param port port we're trying to connect to
+   * @return returns true if connection succeeded, returns false otherwise
+   */
+  public boolean initialize(String ip, Integer port) {
     userSocket = new Socket();
-    userInput = new Scanner(userSocket.getInputStream());
-    Socket serverSocket = new Socket("localhost", 4999);
-    handler = new ServerHandler(serverSocket);
-    String type = handler.getResponse();
-    GameInitializer initializer = new GameInitializer(BoardTypes.valueOf(type), userSocket.getOutputStream());
-    board = initializer.getBoard();
-    initializer.initializeGraphic();
+    try {
+      userInput = new Scanner(userSocket.getInputStream());
+      Socket serverSocket = new Socket(ip, port);
+      handler = new ServerHandler(serverSocket);
+      String type = handler.getResponse();
+      GameInitializer initializer = new GameInitializer(BoardTypes.valueOf(type), userSocket.getOutputStream());
+      board = initializer.getBoard();
+      initializer.initializeGraphic();
+      initialized = true;
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
   }
 
+  /**
+   * use this function to start the main game loop, requires previous use of
+   * initialize()
+   */
   public void run() {
-    try {
-      initialize();
-    } catch (Exception e) {
-      JOptionPane.showMessageDialog(null, "Initializing failed, exiting");
+    if (!initialized) {
       return;
     }
-
     processCommand(handler.getResponse());
-    while(keepRunning) {
+    while (keepRunning) {
       String userMove = userInput.nextLine();
       handler.sendCommand(userMove);
       processCommand(handler.getResponse(), userMove);
+    }
 
+    try {
+      userSocket.close();
+      handler.close();
+    } catch (IOException e) {
     }
   }
 
@@ -52,6 +71,5 @@ public class GameRunner {
   }
 
   private void processCommand(String command, String userMove) {
-    
   }
 }
