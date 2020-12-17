fbfc47cda50c70982ac05837cbdeed0944b4bfd2
Mateusz Kmak <hamsterkmak@gmail.com>
2020-12-17 01:36:25 +0100

    implemented GameWindow constructor

--- client/src/main/java/cs/checkers/client/frontend/GameWindow.java
+++ client/src/main/java/cs/checkers/client/frontend/GameWindow.java
@@ -1,12 +1,21 @@
 package cs.checkers.client.frontend;
 
-import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+import cs.checkers.client.frontend.adapter.JPanelAdapter;
+import java.awt.BorderLayout;
 
 /**
  * houses graphic for {@link cs.checkers.client.board.VisualBoard}
  */
-public class GameWindow extends JPanel {
+public class GameWindow extends JFrame {
 
+  public GameWindow(JPanelAdapter board) {
+    this.setName("Chinese Checkers");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(new BorderLayout());
+    this.add(board, BorderLayout.CENTER);
+  }
 	/**
 	 *
 	 */
