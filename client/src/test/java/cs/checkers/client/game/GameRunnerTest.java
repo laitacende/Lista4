ce87326b92dc815afe0da31bd0c12487f12fd72a
Mateusz Kmak <hamsterkmak@gmail.com>
2021-01-10 21:37:16 +0100

    made GameRunnerTest do something

--- client/src/test/java/cs/checkers/client/game/GameRunnerTest.java
+++ client/src/test/java/cs/checkers/client/game/GameRunnerTest.java
@@ -2,22 +2,16 @@ package cs.checkers.client.game;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * GameRunnerTest
  */
 public class GameRunnerTest {
-  @Disabled
   @Test
   public void initializingGoesWrong() {
     GameRunner runner = new GameRunner();
-    assertTrue(!runner.initialize("8.8.8", 4999));
+    assertTrue(!runner.initialize("xd", 1));
   }
 
-  // I will probably have to create a mock server in another thread
-  // to do this test, I will do it later
-  public void initializingGoesRight() {
-  }
 }
