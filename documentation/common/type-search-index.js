7ead3a3f88a0b2f4a4f0933d38d856506627ab7c
Mateusz Kmak <hamsterkmak@gmail.com>
2021-01-11 14:05:51 +0100

    added documentation

---
+++ documentation/common/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"cs.checkers.common","l":"BoardTypes"},{"p":"cs.checkers.common","l":"CommandParser"}]
---
+++ documentation/server/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cs.checkers"},{"l":"cs.checkers.gamelogic.board"},{"l":"cs.checkers.gamelogic.checker"},{"l":"cs.checkers.gamelogic.field"},{"l":"cs.checkers.gamelogic.move"},{"l":"cs.checkers.gamelogic.moveset"},{"l":"cs.checkers.gamelogic.movevalidator"},{"l":"cs.checkers.server"}]
